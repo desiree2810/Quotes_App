b541efabd9135c31f7d2928cfe5d71ca919e2fcb
MayurNaik21 <mayurnaik2104@gmail.com>
2023-12-29 18:50:07 +0530

    uncommented update quote

--- quotes_backend/src/quotes/quotes.service.ts
+++ quotes_backend/src/quotes/quotes.service.ts
@@ -337,12 +337,12 @@ async countLikesAndDislikes(): Promise<void> {
     // console.log()
 
 
-    // eachQuote.like = likedCount;
-    // eachQuote.dislikes = dislikedCount;
+    eachQuote.like = likedCount;
+    eachQuote.dislikes = dislikedCount;
     // console.log("-----------------------", eachQuote.like)
     // console.log("-----------------------", eachQuote.dislikes)
 
-    // await this.quoteRepository.save(quote);
+    await this.quoteRepository.save(eachQuote);
 }
 }
 
